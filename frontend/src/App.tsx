014741553e6f4d0d7706d2d96965cd056735d5e2
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-07-26 16:06:46 +0300

    feat: simple Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import UserList from "./components/CharacterList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <div className="min-w-screen min-h-screen flex flex-col p-10">
+      <Navbar />
+      <div className="min-w-screen min-h-screen flex flex-col p-10 pt-25">
         <UserList />
       </div>
     </>
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <header className="flex items-center justify-center gap-10 fixed top-0 left-0 w-screen bg-gradient-to-br from-blue-400 to-blue-500 py-5">
+      <h1 className="font-bold text-3xl">
+        Browse your favorite DC and Marvel Hero
+      </h1>
+    </header>
+  );
+}
+
+export default Navbar;
